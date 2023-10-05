d912854a21d1524dcc70703f104b2bcd75c42fbf
Marcelogr04 <122291630+Marcelogr04@users.noreply.github.com>
2023-10-05 20:17:41 +0200

    Create ex41pagina1.php

---
+++ ex41pagina1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 1</title>
+</head>
+<body>
+    <h1>REGISTRA NOMBRE</h1>
+    <form method="POST" action="ex41pagina2.php">
+        <input type="number" name="ocult">
+        <button>Enviar</button>
+    </form>
+</body>
+</html>
